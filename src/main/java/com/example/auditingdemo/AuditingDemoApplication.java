625f628e3089550dfa85adb7341607fbb588f7ab
JayChen <JayChen@cathayholdings.com.tw>
2025-04-02 10:40:13 +0800

    first commit

---
+++ src/main/java/com/example/auditingdemo/AuditingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.auditingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auditingdemo/AuditingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auditingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
