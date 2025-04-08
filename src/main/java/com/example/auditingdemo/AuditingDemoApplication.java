e55f129184a0800dea28fda2f369f949e20c7e0c
JayChen <JayChen@cathayholdings.com.tw>
2025-04-08 09:00:11 +0800

    Remove @EnableJpaAuditing annotation from application.
    The @EnableJpaAuditing annotation was removed as it is no longer necessary in this class. Auditing configuration can now be handled elsewhere, streamlining the application setup.

--- src/main/java/com/example/auditingdemo/AuditingDemoApplication.java
+++ src/main/java/com/example/auditingdemo/AuditingDemoApplication.java
@@ -3,10 +3,8 @@ package com.example.auditingdemo;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class AuditingDemoApplication {
 
     public static void main(String[] args) {
